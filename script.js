0072cfc3858a07fbb65880d4b549b4f18e36b5fd
Sanjay Rautela <140192191+sanjayrautela@users.noreply.github.com>
2024-01-19 17:53:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function () {
+            // DOM is loaded, let's write the string
+            document.body.innerHTML = 'DOM load success';
+        });
